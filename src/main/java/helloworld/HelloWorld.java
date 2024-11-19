4313bd6abc88cf3a5edfa3f5c7ab815cd9aa53ec
DAY0522 <ekdud_____@naver.com>
2024-11-19 14:51:00 +0900

    commit init

--- src/main/java/helloworld/HelloWorld.java
+++ src/main/java/helloworld/HelloWorld.java
@@ -1,6 +1,6 @@
-package org.example;
+package helloworld;
 
-public class Main {
+public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
